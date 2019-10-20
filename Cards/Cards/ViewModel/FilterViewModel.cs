8b19e0ca0021907cbd445fa297a91a8e5acc4234
Daria <daria40tim@gmail.com>
2019-10-20 21:00:45 +0300

    Финальная версия поиска

--- Cards/Cards/ViewModel/FilterViewModel.cs
+++ Cards/Cards/ViewModel/FilterViewModel.cs
@@ -17,138 +17,138 @@ namespace Cards.ViewModel
             string[] _types = { "ОСМ", "ОСЗМ", "ОСМ1", "ОСВМ", "ТСМ", "ТСЗМ", "ТСМ1", "ТСВМ"};
             Types = new List<string>();
             Types.AddRange(_types);
-            SearchCommand = new BaseCommand(SearchMethod);
+            //SearchCommand = new BaseCommand(SearchMethod);
         }
 
-        private void SearchMethod()
-        {
-            string typo = SelectedType.ToString();
-            string pow = Power;
-            string first = Pr_Voltage;
-            string sec = Sec_Voltage;
-            string shield = Is_Schielded.ToString();
-            string bid = Bid.ToString();
-            string test = Is_Not_Tested.ToString();
-            string conn = Conn_Type;
-            string coils = Coil_Num.ToString();
-            string mesh = Measure;
-            string searchStr = "select card_id, tr_type, tr_power, pr_voltage, sec_voltage, is_shielded, bd_date, author, bid, is_not_tested, picture, card_file, addition, conn_type, coil_num, measure from tp_card where";
-            if (Power != null) { searchStr += " (tr_power = " + pow + ") and "; }
-            else { pow = ""; }
-            if (Pr_Voltage != null) { searchStr += " (pr_voltage = '" + first + "') and "; }
-            else { first = ""; }
-            if (Sec_Voltage != null) { searchStr += " (sec_voltage = '" + sec + "') and "; }
-            else { sec = ""; }
-            if (shield != null) { searchStr += " (is_shielded = " + shield + ") and "; }
-            else { shield = ""; }
-            if (bid != null) { searchStr += " (bid = '" + bid + "') and "; }
-            else { bid = ""; }
-            if (test != null) { searchStr += " (is_not_tested = " + test + ") and"; }
-            else { test = ""; }
-            if (conn != null) { searchStr += " (conn_type = '" + conn + "') and "; }
-            else { conn = ""; }
-            if (coils != null) { searchStr += " (coil_num = " + coils + ") and "; }
-            else { coils = ""; }
-            if (mesh != null) { searchStr += " (measure = '" + mesh + "') and"; }
-            else { mesh = ""; }
-            if (SelectedType != null) { searchStr += " (tr_type = '" + typo + "')"; }
-            else { typo = ""; }
-            CardCollection = Collection.ReadCards(searchStr);
-        }
+        //private void SearchMethod()
+        //{
+        //    string typo = SelectedType.ToString();
+        //    string pow = Power;
+        //    string first = Pr_Voltage;
+        //    string sec = Sec_Voltage;
+        //    string shield = Is_Schielded.ToString();
+        //    string bid = Bid.ToString();
+        //    string test = Is_Not_Tested.ToString();
+        //    string conn = Conn_Type;
+        //    string coils = Coil_Num.ToString();
+        //    string mesh = Measure;
+        //    string searchStr = "select card_id, tr_type, tr_power, pr_voltage, sec_voltage, is_shielded, bd_date, author, bid, is_not_tested, picture, card_file, addition, conn_type, coil_num, measure from tp_card where";
+        //    if (Power != null) { searchStr += " (tr_power = " + pow + ") and "; }
+        //    else { pow = ""; }
+        //    if (Pr_Voltage != null) { searchStr += " (pr_voltage = '" + first + "') and "; }
+        //    else { first = ""; }
+        //    if (Sec_Voltage != null) { searchStr += " (sec_voltage = '" + sec + "') and "; }
+        //    else { sec = ""; }
+        //    if (shield != null) { searchStr += " (is_shielded = " + shield + ") and "; }
+        //    else { shield = ""; }
+        //    if (bid != null) { searchStr += " (bid = '" + bid + "') and "; }
+        //    else { bid = ""; }
+        //    if (test != null) { searchStr += " (is_not_tested = " + test + ") and"; }
+        //    else { test = ""; }
+        //    if (conn != null) { searchStr += " (conn_type = '" + conn + "') and "; }
+        //    else { conn = ""; }
+        //    if (coils != null) { searchStr += " (coil_num = " + coils + ") and "; }
+        //    else { coils = ""; }
+        //    if (mesh != null) { searchStr += " (measure = '" + mesh + "') and"; }
+        //    else { mesh = ""; }
+        //    if (SelectedType != null) { searchStr += " (tr_type = '" + typo + "')"; }
+        //    else { typo = ""; }
+        //    CardCollection = Collection.ReadCards(searchStr);
+        //}
 
-        public string Power
-        {
-            get { return (string)GetValue(PowerProperty); }
-            set { SetValue(PowerProperty, value); }
-        }
+        //public string Power
+        //{
+        //    get { return (string)GetValue(PowerProperty); }
+        //    set { SetValue(PowerProperty, value); }
+        //}
 
-        // Using a DependencyProperty as the backing store for Power.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty PowerProperty =
-            DependencyProperty.Register("Power", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
-
-
-        public string Pr_Voltage
-        {
-            get { return (string)GetValue(Pr_VoltageProperty); }
-            set { SetValue(Pr_VoltageProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for Pr_Voltage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty Pr_VoltageProperty =
-            DependencyProperty.Register("Pr_Voltage", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
-        
-        public string Sec_Voltage
-        {
-            get { return (string)GetValue(Sec_VoltageProperty); }
-            set { SetValue(Sec_VoltageProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for Sec_Voltage.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty Sec_VoltageProperty =
-            DependencyProperty.Register("Sec_Voltage", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
-        
-        public bool Is_Schielded
-        {
-            get { return (bool)GetValue(Is_SchieldedProperty); }
-            set { SetValue(Is_SchieldedProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for Is_Schielded.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty Is_SchieldedProperty =
-            DependencyProperty.Register("Is_Schielded", typeof(bool), typeof(FilterViewModel), new PropertyMetadata(null));
-        
-        public bool Bid
-        {
-            get { return (bool)GetValue(BidProperty); }
-            set { SetValue(BidProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for Bid.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty BidProperty =
-            DependencyProperty.Register("Bid", typeof(bool), typeof(FilterViewModel), new PropertyMetadata(null));
+        //// Using a DependencyProperty as the backing store for Power.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty PowerProperty =
+        //    DependencyProperty.Register("Power", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
 
 
-        public bool Is_Not_Tested
-        {
-            get { return (bool)GetValue(Is_Not_TestedProperty); }
-            set { SetValue(Is_Not_TestedProperty, value); }
-        }
+        //public string Pr_Voltage
+        //{
+        //    get { return (string)GetValue(Pr_VoltageProperty); }
+        //    set { SetValue(Pr_VoltageProperty, value); }
+        //}
 
-        // Using a DependencyProperty as the backing store for Is_Not_Tested.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty Is_Not_TestedProperty =
-            DependencyProperty.Register("Is_Not_Tested", typeof(bool), typeof(FilterViewModel), new PropertyMetadata(null));
+        //// Using a DependencyProperty as the backing store for Pr_Voltage.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty Pr_VoltageProperty =
+        //    DependencyProperty.Register("Pr_Voltage", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
 
-        public string Conn_Type
-        {
-            get { return (string)GetValue(Conn_TypeProperty); }
-            set { SetValue(Conn_TypeProperty, value); }
-        }
+        //public string Sec_Voltage
+        //{
+        //    get { return (string)GetValue(Sec_VoltageProperty); }
+        //    set { SetValue(Sec_VoltageProperty, value); }
+        //}
 
-        // Using a DependencyProperty as the backing store for Conn_Type.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty Conn_TypeProperty =
-            DependencyProperty.Register("Conn_Type", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
+        //// Using a DependencyProperty as the backing store for Sec_Voltage.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty Sec_VoltageProperty =
+        //    DependencyProperty.Register("Sec_Voltage", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
+
+        //public bool Is_Schielded
+        //{
+        //    get { return (bool)GetValue(Is_SchieldedProperty); }
+        //    set { SetValue(Is_SchieldedProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for Is_Schielded.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty Is_SchieldedProperty =
+        //    DependencyProperty.Register("Is_Schielded", typeof(bool), typeof(FilterViewModel), new PropertyMetadata(null));
+
+        //public bool Bid
+        //{
+        //    get { return (bool)GetValue(BidProperty); }
+        //    set { SetValue(BidProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for Bid.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty BidProperty =
+        //    DependencyProperty.Register("Bid", typeof(bool), typeof(FilterViewModel), new PropertyMetadata(null));
 
 
-        public int Coil_Num
-        {
-            get { return (int)GetValue(Coil_NumProperty); }
-            set { SetValue(Coil_NumProperty, value); }
-        }
+        //public bool Is_Not_Tested
+        //{
+        //    get { return (bool)GetValue(Is_Not_TestedProperty); }
+        //    set { SetValue(Is_Not_TestedProperty, value); }
+        //}
 
-        // Using a DependencyProperty as the backing store for Coil_Num.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty Coil_NumProperty =
-            DependencyProperty.Register("Coil_Num", typeof(int), typeof(FilterViewModel), new PropertyMetadata(null));
+        //// Using a DependencyProperty as the backing store for Is_Not_Tested.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty Is_Not_TestedProperty =
+        //    DependencyProperty.Register("Is_Not_Tested", typeof(bool), typeof(FilterViewModel), new PropertyMetadata(null));
+
+        //public string Conn_Type
+        //{
+        //    get { return (string)GetValue(Conn_TypeProperty); }
+        //    set { SetValue(Conn_TypeProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for Conn_Type.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty Conn_TypeProperty =
+        //    DependencyProperty.Register("Conn_Type", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
 
 
-        public string Measure
-        {
-            get { return (string)GetValue(MeasureProperty); }
-            set { SetValue(MeasureProperty, value); }
-        }
+        //public int Coil_Num
+        //{
+        //    get { return (int)GetValue(Coil_NumProperty); }
+        //    set { SetValue(Coil_NumProperty, value); }
+        //}
 
-        // Using a DependencyProperty as the backing store for Measure.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty MeasureProperty =
-            DependencyProperty.Register("Measure", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
+        //// Using a DependencyProperty as the backing store for Coil_Num.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty Coil_NumProperty =
+        //    DependencyProperty.Register("Coil_Num", typeof(int), typeof(FilterViewModel), new PropertyMetadata(null));
+
+
+        //public string Measure
+        //{
+        //    get { return (string)GetValue(MeasureProperty); }
+        //    set { SetValue(MeasureProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for Measure.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty MeasureProperty =
+        //    DependencyProperty.Register("Measure", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
 
 
         public List<string> Types
@@ -162,27 +162,27 @@ namespace Cards.ViewModel
             DependencyProperty.Register("Types", typeof(List<string>), typeof(FilterViewModel), new PropertyMetadata(null));
 
 
-        public string SelectedType
-        {
-            get { return (string)GetValue(SelectedTypeProperty); }
-            set { SetValue(SelectedTypeProperty, value); }
-        }
+        //public string SelectedType
+        //{
+        //    get { return (string)GetValue(SelectedTypeProperty); }
+        //    set { SetValue(SelectedTypeProperty, value); }
+        //}
 
-        // Using a DependencyProperty as the backing store for SelectedType.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty SelectedTypeProperty =
-            DependencyProperty.Register("SelectedType", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
+        //// Using a DependencyProperty as the backing store for SelectedType.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty SelectedTypeProperty =
+        //    DependencyProperty.Register("SelectedType", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
 
 
 
-        public BaseCommand SearchCommand
-        {
-            get { return (BaseCommand)GetValue(SearchCommandProperty); }
-            set { SetValue(SearchCommandProperty, value); }
-        }
+        //public BaseCommand SearchCommand
+        //{
+        //    get { return (BaseCommand)GetValue(SearchCommandProperty); }
+        //    set { SetValue(SearchCommandProperty, value); }
+        //}
 
-        // Using a DependencyProperty as the backing store for SearchCommand.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty SearchCommandProperty =
-            DependencyProperty.Register("SearchCommand", typeof(BaseCommand), typeof(FilterViewModel), new PropertyMetadata(null));
+        //// Using a DependencyProperty as the backing store for SearchCommand.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty SearchCommandProperty =
+        //    DependencyProperty.Register("SearchCommand", typeof(BaseCommand), typeof(FilterViewModel), new PropertyMetadata(null));
 
 
 
@@ -194,11 +194,7 @@ namespace Cards.ViewModel
 
         // Using a DependencyProperty as the backing store for CardCollection.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CardCollectionProperty =
-            DependencyProperty.Register("CardCollection", typeof(ObservableCollection<Card>), typeof(FilterViewModel), new PropertyMetadata(null, OnCardCollectionChanged));
+            DependencyProperty.Register("CardCollection", typeof(ObservableCollection<Card>), typeof(FilterViewModel), new PropertyMetadata(null));
 
-        private static void OnCardCollectionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            Collection.Cards = (ObservableCollection<Card>)e.NewValue;
-        }
     }
 }
--- Cards/Cards/ViewModel/MainWindowViewModel.cs
+++ Cards/Cards/ViewModel/MainWindowViewModel.cs
@@ -12,11 +12,15 @@ namespace Cards.ViewModel
 {
     public class MainWindowViewModel: DependencyObject
     {
+        static string query = "";
+
         public MainWindowViewModel()
         {
+            query = "select card_id, tr_type, tr_power, pr_voltage, sec_voltage, is_shielded, bd_date, author, bid, is_not_tested, picture, card_file, addition, conn_type, coil_num, measure from tp_card where";
             Collection.FillData();
             DataGridVM = new DataGridViewModel();
             FilterVM = new FilterViewModel();
+            SelectedType = FilterVM.Types[0];
             CardsCollection = Collection.Cards;
             SelectedCard = DataGridVM.SelectedCard;
             InitializeCommands();
@@ -25,6 +29,89 @@ namespace Cards.ViewModel
         private void InitializeCommands()
         {
             OpenFileCommand = new BaseCommand(OpenFileMethod);
+            SearchCommand = new BaseCommand(SearchMethod);
+        }
+
+        private void SearchMethod()
+        {
+            if (Power != null) { query += " (tr_power = " + Power + ") and "; }
+            //if (Pr_Voltage != null) { query += " (pr_voltage = '" + Pr_Voltage + "') and "; }
+            //if (Sec_Voltage != null) { query += " (sec_voltage = '" + Sec_Voltage + "') and "; }
+            if (Is_Schielded != null) { query += " (is_shielded = " + Is_Schielded + ") and "; }
+            if (Bid != null) { query += " (bid = '" + Bid + "') and "; }
+            if (Is_Not_Tested != null) { query += " (is_not_tested = " + Is_Not_Tested + ") and"; }
+            if (Conn_Type != null) { query += " (conn_type = '" + Conn_Type + "') and "; }
+            if (Coil_Num != 0) { query += " (coil_num = " + Coil_Num + ") and "; }
+            if (Measure != null) { query += " (measure = '" + Measure + "') and"; }
+            if (SelectedType != null) { query += " (tr_type = '" + SelectedType + "')"; }
+            try
+                {
+                    Collection.Cards = Collection.ReadCards(query);
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show("Произошла ошибка. Проверьте введенные данные.");
+                    Collection.Cards = Collection.ReadCards("");
+                }
+            if (Pr_Voltage != "" && Sec_Voltage != "") { Collection.Cards = FindPrVoltage(Pr_Voltage, Sec_Voltage); }
+            if (Collection.Cards.Count == 0)
+            {
+                MessageBox.Show("Не найдено. Введите меньше параметров и попытайтесь осуществить поиск вручную");
+            }
+            else { DataGridVM = new DataGridViewModel(); }
+            query = "select card_id, tr_type, tr_power, pr_voltage, sec_voltage, is_shielded, bd_date, author, bid, is_not_tested, picture, card_file, addition, conn_type, coil_num, measure from tp_card where";
+            //MessageBox.Show(query);
+        }
+
+        private ObservableCollection<Card> FindPrVoltage(string pr, string sec)
+        {
+            ObservableCollection<Card> res = new ObservableCollection<Card>(); 
+            string[] volt;
+            Voltage a = null;
+            if (pr != null)
+            {
+                volt = pr.Split('-');
+                a = new Voltage(volt);
+            }
+            List<Voltage> b = new List<Voltage>();
+            if (sec != null)
+            {
+                volt = sec.Split('/');
+                b = new List<Voltage>();
+                foreach (var item in volt)
+                {
+                    Voltage c = new Voltage(item.Split('-'));
+                    b.Add(c);
+                }
+            }
+            bool contains = false;
+            foreach (var item in Collection.Cards)
+            {
+                foreach (var i in a.Values)
+                {
+                    contains = item.Pr_Voltage.Values.Contains(i);
+                    if (!contains) { break; }
+                }
+                if (contains) { res.Add(item); }
+            }
+            contains = false;
+            ObservableCollection<Card> res1 = new ObservableCollection<Card>();
+            foreach (var item in res)
+            {
+                foreach (var iter in item.Sec_Voltage)
+                {
+                    foreach (var intel in b)
+                    {
+                        foreach (var intellij in intel.Values)
+                        {
+                            contains = iter.Values.Contains(intellij);
+                            if (!contains) { break; }
+                        }
+                    }
+                }
+                if (contains) { res1.Add(item); }
+            }
+            return res1;
         }
 
         private void OpenFileMethod()
@@ -107,6 +194,113 @@ namespace Cards.ViewModel
         // Using a DependencyProperty as the backing store for CardsCollection.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty CardsCollectionProperty =
             DependencyProperty.Register("CardsCollection", typeof(ObservableCollection<Card>), typeof(MainWindowViewModel), new PropertyMetadata(null));
+
+    
+        public string Power
+        {
+            get { return (string)GetValue(PowerProperty); }
+            set { SetValue(PowerProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Power.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PowerProperty =
+            DependencyProperty.Register("Power", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
+
+
+        public string Pr_Voltage
+        {
+            get { return (string)GetValue(Pr_VoltageProperty); }
+            set { SetValue(Pr_VoltageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Pr_Voltage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty Pr_VoltageProperty =
+            DependencyProperty.Register("Pr_Voltage", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
+
+        public string Sec_Voltage
+        {
+            get { return (string)GetValue(Sec_VoltageProperty); }
+            set { SetValue(Sec_VoltageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Sec_Voltage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty Sec_VoltageProperty =
+            DependencyProperty.Register("Sec_Voltage", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
+
+        public bool Is_Schielded
+        {
+            get { return (bool)GetValue(Is_SchieldedProperty); }
+            set { SetValue(Is_SchieldedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Is_Schielded.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty Is_SchieldedProperty =
+            DependencyProperty.Register("Is_Schielded", typeof(bool), typeof(FilterViewModel), new PropertyMetadata(null));
+
+        public bool Bid
+        {
+            get { return (bool)GetValue(BidProperty); }
+            set { SetValue(BidProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Bid.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BidProperty =
+            DependencyProperty.Register("Bid", typeof(bool), typeof(FilterViewModel), new PropertyMetadata(null));
+
+
+        public bool Is_Not_Tested
+        {
+            get { return (bool)GetValue(Is_Not_TestedProperty); }
+            set { SetValue(Is_Not_TestedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Is_Not_Tested.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty Is_Not_TestedProperty =
+            DependencyProperty.Register("Is_Not_Tested", typeof(bool), typeof(FilterViewModel), new PropertyMetadata(null));
+
+        public string Conn_Type
+        {
+            get { return (string)GetValue(Conn_TypeProperty); }
+            set { SetValue(Conn_TypeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Conn_Type.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty Conn_TypeProperty =
+            DependencyProperty.Register("Conn_Type", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
+
+
+        public int Coil_Num
+        {
+            get { return (int)GetValue(Coil_NumProperty); }
+            set { SetValue(Coil_NumProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Coil_Num.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty Coil_NumProperty =
+            DependencyProperty.Register("Coil_Num", typeof(int), typeof(FilterViewModel), new PropertyMetadata(null));
+
+
+        public string Measure
+        {
+            get { return (string)GetValue(MeasureProperty); }
+            set { SetValue(MeasureProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Measure.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MeasureProperty =
+            DependencyProperty.Register("Measure", typeof(string), typeof(FilterViewModel), new PropertyMetadata(null));
+        
+
+        public BaseCommand SearchCommand
+        {
+            get { return (BaseCommand)GetValue(SearchCommandProperty); }
+            set { SetValue(SearchCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SearchCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SearchCommandProperty =
+            DependencyProperty.Register("SearchCommand", typeof(BaseCommand), typeof(FilterViewModel), new PropertyMetadata(null));
+
 
 
     }
